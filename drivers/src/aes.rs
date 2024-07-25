19fd38c168d4f1a3241108b3a487c17bac7a7ae8
gephaistos <olehmaksa@gmail.com>
2024-07-25 12:12:06 +0300

    Connect `forticrypt_hal`

---
+++ drivers/src/aes.rs
@@ -0,0 +1,2 @@
+pub use forticrypt_hal::{Peripherals as AesPeripherals, Op as AesOp, Mode as AesMode};
+pub use forticrypt_hal::{Aes128, Aes192, Aes256};
--- drivers/src/lib.rs
+++ drivers/src/lib.rs
@@ -18,6 +18,7 @@ mod array;
 mod array_concat;
 mod wait;
 
+mod aes;
 mod bounded_address;
 mod csrng;
 mod data_vault;
@@ -52,6 +53,7 @@ mod trng_ext;
 
 pub use array::{Array4x12, Array4x4, Array4x5, Array4x8, Array4xN};
 pub use array_concat::array_concat3;
+pub use aes::{Aes128, Aes192, Aes256, AesPeripherals, AesOp, AesMode};
 pub use bounded_address::{BoundedAddr, MemBounds, RomAddr};
 pub use caliptra_error::{CaliptraError, CaliptraResult};
 pub use csrng::{Csrng, HealthFailCounts as CsrngHealthFailCounts, Seed as CsrngSeed};
